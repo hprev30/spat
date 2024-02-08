135a46cab847c6bf93ef32c7ccd4204c9aa7c434
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2024-02-07 22:10:09 -0500

    updated models to include offset based on soak days

--- R/03_models.R
+++ R/03_models.R
@@ -9,21 +9,25 @@ source(here('R', '01_load-tidy-spat.R'))
 # generalized linear models -----------------------------------------------
 spat2 <- spat %>% mutate(year = factor(year)) %>% rename(region = region_friendly) # make year a factor and rename region to shorten
 
-# region
-mod.0 <- glmmTMB::glmmTMB(spat_std ~ region, data = spat2, family = "nbinom2")
-summary(mod.0)
+# intercept only
+mod.0 <- glmmTMB::glmmTMB(spat_count ~ offset(log(soak_time_days)), data = spat2, family = "nbinom2")
+summary(mod)
 
-# region + year
-mod.1 <- update(mod.0, . ~. + year)
+# region
+mod.1 <- update(mod.0, .~. + region)
 summary(mod.1)
 
-# region*year
-mod.2 <- update(mod.1, .~. + region:year)
+# region + year
+mod.2 <- update(mod.1, . ~. + year)
 summary(mod.2)
 
-# year
-mod.3 <- glmmTMB::glmmTMB(spat_std ~ year, data = spat2, family = "nbinom2")
+# region*year
+mod.3 <- update(mod.2, .~. + region:year)
 summary(mod.3)
+
+# year
+mod.4 <- update(mod.0, .~. + year)
+summary(mod.4)
 
 # check out the data
 ggplot(spat2, aes(x = region, y = spat_std)) +
@@ -45,8 +49,8 @@ ggplot(spat2, aes(x = region, y = spat_std, group = region)) +
 # model selection via AIC -------------------------------------------------
 
 # merge models into one list
-modset = list(mod.0,mod.1,mod.2,mod.3)
-modnames2 = c("spat ~ region", "spat ~ region + year", "spat ~ region + year + region * year", "spat ~ year")
+modset = list(mod.0,mod.1,mod.2,mod.3, mod.4)
+modnames2 = c("intercept", "spat ~ region", "spat ~ region + year", "spat ~ region + year + region * year", "spat ~ year")
 names(modset) <- modnames2
 
 #AIC(c) table of all models
@@ -55,17 +59,21 @@ names(modset) <- modnames2
 bbmle::AICctab(modset, weights = TRUE)
 AICcmodavg::aictab(modset, modnames2, second.ord = TRUE) #model selection table with AICc
 
-performance::check_model(mod.1)
+performance::check_model(mod.2)
 
 # model diagnostics -------------------------------------------------------
 
 # classic ANOVA style table for output (but uses Chi-Square tests)
 car::Anova(mod.2)
+# interaction
+car::Anova(mod.3)
+
+Anova.glmmTMB(mod.2)
 
 # test for autocorrelation on full model
-res1 <- simulateResiduals(mod.1)
+res1 <- simulateResiduals(mod.2)
 plot(res1)
-performance::check_autocorrelation(mod.1)
+performance::check_autocorrelation(mod.2)
 
 simulationOutput1 = recalculateResiduals(res1, group = spat2$region)
 plot(simulationOutput1)
@@ -77,14 +85,14 @@ plot(simulationOutput2)
 
 # check for overdispersion
 # 
-performance::check_overdispersion(mod.1)
+performance::check_overdispersion(mod.2)
 
 
 
 # quantify differences ----------------------------------------------------
 # estimated marginal means (least-squares means)
 
-m.region <- emmeans(mod.1, ~ region)
+m.region <- emmeans(mod.2, ~ region)
 m.region
 
 pairs(m.region, adjust = "tukey")
