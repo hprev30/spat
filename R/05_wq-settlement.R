96fe55c3aaf03374535c9e6e0f3dab8e500c5f3a
Shannon Dunnigan Kimmel <Shannon.Dunnigan@dep.state.fl.us>
2024-02-09 14:37:05 -0500

    created script for wq in.out of settlement period

---
+++ R/05_wq-settlement.R
@@ -0,0 +1,92 @@
+# Summary statistics of water quality parameters inside and outside of spat settlement periods
+
+# library(here)
+# source(here('R', '00_load-packages.R'))
+# source(here('R', '00_vis-custom.R'))
+# source(here('R', '02_load-tidy-NUT-WQ.R'))
+
+
+# 05 settlement periods ---------------------------------------------------
+
+settlement <- tribble(
+  ~month, ~settlement,
+  'Jan', 'None',
+  'Feb','None',
+  'Mar', 'None',
+  'Apr', 'Yes',
+  'May', 'Yes',
+  'Jun', 'Yes', 
+  'Jul', 'Yes',
+  'Aug', 'Yes', 
+  'Sep', 'Yes', 
+  'Oct', 'Yes',
+  'Nov', 'None',
+  'Dec', 'None'
+)
+
+pi_1 <- pi %>% 
+  left_join(pi_chla, by = "datetimestamp") %>% 
+  select(datetimestamp, temp, sal, turb, chla_n) %>% 
+  mutate(month = lubridate::month(datetimestamp, label = T)) %>% 
+  left_join(settlement, by = "month") %>% 
+  mutate(station = "piwq")
+
+ss_1 <- ss %>% 
+  left_join(ss_chla, by = "datetimestamp") %>% 
+  select(datetimestamp, temp, sal, turb, chla_n) %>%
+  mutate(month = lubridate::month(datetimestamp, label = T)) %>% 
+  left_join(settlement, by = "month") %>% 
+  mutate(station = "sswq")
+
+fm_1 <- fm %>% 
+  left_join(fm_chla, by = "datetimestamp") %>% 
+  select(datetimestamp, temp, sal, turb, chla_n) %>%
+  mutate(month = lubridate::month(datetimestamp, label = T)) %>% 
+  left_join(settlement, by = "month") %>% 
+  mutate(station = "fmwq")
+
+# settlement_WQ <- bind_rows(pi_1, ss_1, fm_1) %>% 
+#   group_by(station, settlement) 
+
+pi_1 %>% 
+  split(.$settlement) %>% 
+  purrr::map(summary)
+
+ss_1 %>% 
+  split(.$settlement) %>% 
+  purrr::map(summary)
+
+fm_1 %>% 
+  split(.$settlement) %>% 
+  purrr::map(summary)
+
+# standard errors
+
+fun_se <- function(x) (sd(x, na.rm = T)/sqrt(length(x)))
+
+pi_1 %>% 
+  select(settlement, temp, sal, turb, chla_n) %>% 
+  group_by(settlement) %>% 
+  summarise(temp = fun_se(temp),
+            sal = fun_se(sal),
+            turb = fun_se(turb),
+            chl = fun_se(chla_n))
+
+ss_1 %>% 
+  select(settlement, temp, sal, turb, chla_n) %>% 
+  group_by(settlement) %>% 
+  summarise(temp = fun_se(temp),
+            sal = fun_se(sal),
+            turb = fun_se(turb),
+            chl = fun_se(chla_n))
+
+fm_1 %>% 
+  select(settlement, temp, sal, turb, chla_n) %>% 
+  group_by(settlement) %>% 
+  summarise(temp = fun_se(temp),
+            sal = fun_se(sal),
+            turb = fun_se(turb),
+            chl = fun_se(chla_n))
+# extra section -----------------------------------------------------------
+
+
